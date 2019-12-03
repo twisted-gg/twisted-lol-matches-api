bf747f047930de5d1abf5ba0e67969165c19ecea
Sansossio <juliosansossio@gmail.com>
2019-12-03 17:47:12 +0100

    Empty project

---
+++ src/main/kotlin/com/twisted/lolmatches/LolMatchesApplication.kt
@@ -0,0 +1,11 @@
+package com.twisted.lolmatches
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LolMatchesApplication
+
+fun main(args: Array<String>) {
+	runApplication<LolMatchesApplication>(*args)
+}
---
+++ src/test/kotlin/com/twisted/lolmatches/LolMatchesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.twisted.lolmatches
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LolMatchesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
